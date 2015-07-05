f3625aa894ec26c13467370696652b6b0a18f287
James Pratt-Delzenne <prattjames4@gmail.com>
2015-07-06 00:47:39 +0200

    smooth animation

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 $(document).ready(function () {
 	$(".ham").click(function () {
 		$(this).toggleClass("close");
+    if ($('.close').val() < 1) {
+      $('.menu').animate({
+        left: '0px'
+      }, 500);
+      $('.button').animate({
+        marginLeft: '300px'
+      }, 500);
+    } else {
+      $('.menu').animate({
+        left: '-300px'
+      }, 500);
+      $('.button').animate({
+        marginLeft: '0px'
+      }, 500);
+    }
 	});
 });
